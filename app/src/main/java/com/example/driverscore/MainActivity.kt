72d336f10c096796da7754f1234d2aa72fc15aec
NEOPIXEL <harusakura10137230@gmail.com>
2023-12-11 15:29:03 +0900

    UIの変更

--- app/src/main/java/com/example/driverscore/MainActivity.kt
+++ app/src/main/java/com/example/driverscore/MainActivity.kt
@@ -22,7 +22,7 @@ import java.util.TimeZone
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.test)
 
         var password = ""
         val errorText = findViewById<TextView>(R.id.error_text)
